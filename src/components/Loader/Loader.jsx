c46da52f3a7a939d02d9d88c606131ebb5a02bc9
Serhii Balaba <balabaserhii@gmail.com>
2024-05-22 21:29:48 +0200

    .

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,23 +1,19 @@
 import { RotatingLines } from "react-loader-spinner";
-import css from "../Loader/Loader.module.css"
+import css from "../Loader/Loader.module.css";
 export const Loader = () => {
   return (
-<div className={css.loader}>
-
-
-
-  <RotatingLines 
-    visible={true}
-    height="96"
-    width="96"
-    color="grey"
-    strokeWidth="5"
-    animationDuration="5.75"
-    ariaLabel="rotating-lines-loading"
-    wrapperStyle={{}}
-    wrapperClass=""
-  />
-</div>
-
+    <div className={css.loader}>
+      <RotatingLines
+        visible={true}
+        height="96"
+        width="96"
+        color="grey"
+        strokeWidth="5"
+        animationDuration="5.75"
+        ariaLabel="rotating-lines-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+      />
+    </div>
   );
 };
